22ef2c9729471aa0718b295650435379de783ad1
ritchey0713 <ritchey0713@gmail.com>
2018-08-19 21:56:52 -0400

    fetch new post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,1 +1,24 @@
 const API_URL = process.env.REACT_APP_API_URL
+
+const addPost = post => {
+  return {
+    type: 'CREATE_NEW_POST',
+    post
+  }
+}
+
+export const createPost = post => {
+  return dispatch => {
+    return fetch(`${API_URL}/posts`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({post: post})
+    })
+    .then(resp => resp.json())
+    .then(post =>{
+      dispatch(addPost(post))
+    })
+  }
+}

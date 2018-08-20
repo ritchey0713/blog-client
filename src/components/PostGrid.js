05db13d0c211869607cb4d1e514ab2f709919e8b
ritchey0713 <ritchey0713@gmail.com>
2018-08-19 22:34:55 -0400

    add transition

--- src/components/PostGrid.js
+++ src/components/PostGrid.js
@@ -12,6 +12,10 @@ class PostGrid extends Component {
             <Link to={`/posts/${post.id}`}>
               <img src={post.img_url} alt={post.caption} />
             </Link>
+            <CSSTransitionGroup transitionName="like" transitionEnterTimeout={500}
+              transitionLeaveTimeout={500}>
+              <span key={post.likes} className="likes-heart">{post.likes}</span>
+            </CSSTransitionGroup>
           </div>
         </div>
       </div>

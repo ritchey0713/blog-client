91fc2ca4a2308442cc3f6a986e27459652c37935
ritchey0713 <ritchey0713@gmail.com>
2018-08-19 21:45:07 -0400

    add form data

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -5,6 +5,21 @@ class PostForm extends Component {
   render(){
     return(
       <div>
+      <h1>Upload a new Post!</h1>
+    <form onSubmit={this.handleOnSubmit}>
+      <input type="text"
+      onChange={this.handleOnChange}
+      ref="title"
+      placeholder="title" />
+      <input type="text"
+      onChange={this.handleOnChange}
+      ref="caption"
+      placeholder="caption" />
+      <input type="text"
+      onChange={this.handleOnChange}
+      ref="img_url"
+      placeholder="img_url" />
+    </form>
       </div>
     )
   }

e52ed33fad25388b0278e61da68c5b3ac71fee09
ritchey0713 <ritchey0713@gmail.com>
2018-08-19 21:43:35 -0400

    add post form

---
+++ src/components/PostForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class PostForm extends Component {
+  render(){
+    return(
+      <div>
+      </div>
+    )
+  }
+}

8c3e55c7d6b1e2600c9b170eb6da5c6255d0bbd8
ritchey0713 <ritchey0713@gmail.com>
2018-08-19 21:28:35 -0400

    add home files

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { BrowserRouter  as Router, Route } from 'react-router-dom'
 import './App.css';
 
+import Home from './containers/Home.js'
+
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
+          <Route exact path='/' component={Home} />
         </div>
-        </Router>
+      </Router>
     );
   }
 }
